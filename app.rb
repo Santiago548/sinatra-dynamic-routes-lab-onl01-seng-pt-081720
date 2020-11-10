ee0829c13238c1e92624a26661883cd061c24ba8
Dominic Barajas <dominicbarajas548@gmail.com>
2020-11-10 23:25:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
+    case params[:operation]
     when "add"
       (num1 + num2).to_s
     when "subtract"
