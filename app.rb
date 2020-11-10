eae60be125eafa81aac2f685a5bcca6ba17cec05
Dominic Barajas <dominicbarajas548@gmail.com>
2020-11-10 23:02:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,12 +14,13 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase}.times"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-  @words = params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]
-  "#{@words}.join()"
+  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
