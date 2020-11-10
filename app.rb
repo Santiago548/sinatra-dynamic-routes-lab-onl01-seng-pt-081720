32e9e99071849e41aaf955dc8206dd4163c10000
Dominic Barajas <dominicbarajas548@gmail.com>
2020-11-10 22:39:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,20 +4,20 @@ class App < Sinatra::Base
   # Write your code here!
 
   get 'reversename/:name' do
-    
+
   end
 
   get '.square/:number' do
-    
+
   end
 
   get '/say/:number/:phrase' do
-    
+
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
   @words = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
   "#{@words}"
   end
-  
+
 end
